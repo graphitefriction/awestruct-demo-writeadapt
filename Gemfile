6c7d4fbab766e1a00ab85d4070903d87d26aa32b
Dan Allen <dan.j.allen@gmail.com>
2013-07-13 15:45:56 -0600

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'awestruct', '>= 0.5.2'
+gem 'rake', '>= 10.0.0'
+gem 'asciidoctor', '>= 0.1.4.pre'
+gem 'coderay'
---
+++ _ext/pipeline.rb
@@ -0,0 +1,9 @@
+require './stylesheets/_themes/lib/functions.rb'
+require 'zurb-foundation'
+
+Awestruct::Extensions::Pipeline.new do
+  # extension Awestruct::Extensions::Posts.new '/news'
+  # extension Awestruct::Extensions::Indexifier.new
+  # Indexifier *must* come before Atomizer
+  # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+end
